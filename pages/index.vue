80f1e53ee6f8f6c27973dd76590b7a544937c22d
sawaryu <sawaryu@sawaryunoMacBook-Pro.local>
2022-02-06 20:22:33 +0900

    need store reset

--- pages/index.vue
+++ pages/index.vue
@@ -10,23 +10,82 @@
 
     <v-col cols="12" md="8">
       <v-card color="rgb(0, 0, 0, 0)" flat>
-        <!-- <v-card-title><v-icon>mdi-home</v-icon>Timeline</v-card-title>
-        <v-divider></v-divider> -->
+        <v-card-title>
+          <v-spacer></v-spacer>
+          <v-btn x-large icon @click="init(true)">
+            <v-icon>mdi-cached</v-icon>
+          </v-btn>
+        </v-card-title>
 
-        <v-card-text v-if="!questions.length" class="text-center">
-          <div class="font-weight-bold text-h6">
-            Questions related to you will be displayed here
-          </div>
-          <div class="text-caption">
-            Let's try create questions or follow people.
-          </div>
-        </v-card-text>
+        <transition name="fade" mode="out-in">
+          <v-row v-if="loading" justify="center" key="loading">
+            <v-col cols="12">
+              <VueLoading
+                type="bars"
+                color="#333"
+                :size="{ width: '50px', height: '50px' }"
+              />
+            </v-col>
+          </v-row>
 
-        <v-row v-else>
-          <v-col v-for="q in questions" :key="q.id" cols="12" sm="6">
-            <Question :question="q" @remove="remove(q.id)" />
-          </v-col>
-        </v-row>
+          <div v-else key="questions">
+            <!-- Nothing -->
+            <v-card-text
+              v-if="!$accessor.questions.getQuestions.length"
+              class="text-center"
+            >
+              <div class="font-weight-bold text-h6">
+                Questions related to you will be displayed here
+              </div>
+              <div class="text-caption">
+                Let's try create questions or follow people.
+              </div>
+            </v-card-text>
+
+            <!-- Questions -->
+            <v-row v-else>
+              <v-col
+                v-for="q in $accessor.questions.getQuestions"
+                :key="q.id"
+                cols="12"
+                sm="6"
+              >
+                <Question :question="q" />
+              </v-col>
+              <v-col cols="12">
+                <infinite-loading
+                  v-if="
+                    $accessor.questions.getPage >= 2 &&
+                    $accessor.questions.getQuestions.length >= 10
+                  "
+                  @infinite="getMore"
+                >
+                  <div slot="spinner">
+                    <VueLoading
+                      type="bars"
+                      color="#333"
+                      :size="{ width: '40px', height: '40px' }"
+                    />
+                  </div>
+                  <div
+                    class="font-weight-bold"
+                    slot="no-more"
+                    style="height: 40px"
+                  >
+                    No more questions.
+                  </div>
+                  <div
+                    class="font-weight-bold"
+                    slot="no-results"
+                    style="height: 40px"
+                  >
+                    No more questions.
+                  </div>
+                </infinite-loading>
+              </v-col>
+            </v-row>
+          </div>
+        </transition>
       </v-card>
     </v-col>
     <FloatAnswer />
@@ -34,21 +93,77 @@
 </template>
 
 <script lang="ts">
-import { Question } from "@/components/common/Question.vue";
 import Vue from "vue";
+import { VueLoading } from "vue-loading-template";
+import InfiniteLoading from "vue-infinite-loading";
 export default Vue.extend({
-  async asyncData({ $axios }) {
-    try {
-      const res = await $axios.$get(`/questions/timeline`);
-      return { questions: res };
-    } catch (error) {}
+  components: {
+    VueLoading,
+    InfiniteLoading,
+  },
+  data() {
+    return {
+      loading: false,
+      scrollY: 0,
+    };
+  },
+  created() {
+    this.init();
+  },
+  destroyed(){
+    // scroll位置を格納する。
+    console.log("Destroyed:", window.scrollY)
   },
   methods: {
-    remove(question_id: number) {
-      this.questions = this.questions.filter(
-        (q: Question) => q.id !== question_id
-      );
+    async init(isReset: boolean = false): Promise<void> {
+      if (isReset) {
+        this.loading = true;
+        this.$accessor.questions.reset();
+      }
+      if (this.$accessor.questions.getPage >= 2) {
+        return;
+      }
+      try {
+        this.loading = true;
+        const res = await this.$axios.$get(`/questions/timeline`, {
+          params: {
+            page: this.$accessor.questions.getPage,
+          },
+        });
+        this.$accessor.questions.incrementPage();
+        console.log(res);
+        this.$accessor.questions.setQuestions(res);
+      } catch (error) {
+      } finally {
+        setTimeout(() => {
+          this.loading = false;
+        }, 250);
+      }
     },
+    async getMore($state: any): Promise<void> {
+      try {
+        const res = await this.$axios.$get(`/questions/timeline`, {
+          params: {
+            page: this.$accessor.questions.getPage,
+          },
+        });
+        console.log(res);
+        setTimeout(() => {
+          if (res.length) {
+            this.$accessor.questions.incrementPage();
+            this.$accessor.questions.pushQuestions(res);
+            $state.loaded();
+          } else {
+            $state.complete();
+          }
+        }, 1000);
+      } catch (e) {
+        console.log(e);
+      }
+    },
+  },
+  beforeDestroy() {
+    console.log(this.$vuetify);
   },
 });
 </script>
--- store/questions.ts
+++ store/questions.ts
@@ -1,18 +1,34 @@
 import { getterTree, mutationTree, actionTree } from 'typed-vuex'
 
+/*
+  * Use in timeline.
+*/
+
 export const state = () => ({
+  page: 1 as number,
   questions: [] as [],
 })
 
 export const getters = getterTree(state, {
+  getPage: (state) => state.page,
   getQuestions: (state) => state.questions,
 })
 
 export const mutations = mutationTree(state, {
+  incrementPage(state) {
+    state.page++;
+  },
+  resetPage(state) {
+    state.page = 1;
+  },
   setQuestions(state, questions: []) {
-    state.questions = questions
+    state.questions = questions;
   },
   pushQuestions(state, questions: []) {
-    state.questions.push(...questions)
+    state.questions.push(...questions);
+  },
+  reset(state) {
+    state.page = 1
+    state.questions = []
   }
 })
