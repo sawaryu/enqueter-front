baa5bf5807e9b240644d8963dc122a1b10f11ddb
sawaryu <sawaryu@sawaryunoMacBook-Pro.local>
2022-02-27 17:43:41 +0900

    fix

--- pages/welcome.vue
+++ pages/welcome.vue
@@ -1,25 +1,23 @@
 <template>
   <div>
-    <div class="d-flex justify-center top-alert">
+    <div class="d-flex justify-center" style="min-height: 74px">
       <v-alert
         v-if="$accessor.alert.getAlert"
         :type="$accessor.alert.getAlert.type"
         outlined
-        class="text-center"
-        style="position:fixed;"
       >
         {{ $accessor.alert.getAlert.message }}
       </v-alert>
     </div>
 
     <div class="d-flex justify-start">
-      <v-card class="pt-12 mt-12" flat color="rgb(0, 0, 0, 0)">
+      <v-card class="mt-10" flat color="rgb(0, 0, 0, 0)">
         <v-card-title class="text-h3 text-sm-h1 font-weight-bold">
           Enqueter
         </v-card-title>
         <v-card-text class="text-subtitle-1 text-sm-h6" style="width: 80vw"
-          >Lorem ipsum dolor sit, amet voluptas
-          incidunt nesciunt illum maxime ut?</v-card-text
+          >Lorem ipsum dolor sit, amet voluptas incidunt nesciunt illum maxime
+          ut?</v-card-text
         >
         <v-card-actions class="d-flex justify-start">
           <v-btn
@@ -55,14 +53,16 @@ export default Vue.extend({
   methods: {
     async init(): Promise<void> {
       // confirmation
-      const confirmId: string | null = (this.$route.query.confirm as string | null);
+      const confirmId: string | null = this.$route.query.confirm as
+        | string
+        | null;
       if (confirmId) {
         this.authConfirm(confirmId);
       }
 
       // password reset
-      const token: string | null = (this.$route.query.token as string | null);
-      const email: string | null = (this.$route.query.email as string | null);
+      const token: string | null = this.$route.query.token as string | null;
+      const email: string | null = this.$route.query.email as string | null;
       if (token && email) {
         this.authResetPassword(token, email);
       }
@@ -102,9 +102,9 @@ export default Vue.extend({
 </script>
 
 <style scoped>
-.top-alert {
+/* .top-alert {
   position: -webkit-sticky;
   position: sticky;
   top: 20px;
-}
+} */
 </style>
